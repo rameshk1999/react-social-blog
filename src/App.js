bd6c158fc80d62c460370d1630a4d4e4fe593840
rameshk1999 <kasularamesh7337@gmail.com>
2021-11-15 15:32:51 +0530

    app protected

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import { Box } from "@mui/system";
-import React from "react";
+import React, { useContext } from "react";
 import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from "./pages/SigninPage";
 import SignUp from "./pages/SignupPage";
 import Profile from "./pages/profile/ProfilePage";
+import { ColorContext } from "./contexts/ColorContext";
 
 const App = () => {
+  const { user } = useContext(ColorContext);
+  console.log(user);
   return (
     <Box>
       <Layout>
         <Routes>
-          <Route exact path="/" element={<HomePage />} />
-          <Route exact path="/signin" element={<SignIn />} />
-          <Route exact path="/signup" element={<SignUp />} />
-          <Route exact path="/profile" element={<Profile />} />
+          {!user ? (
+            <>
+              {" "}
+              <Route exact path="/signin" element={<SignIn />} />
+              <Route exact path="/signup" element={<SignUp />} />{" "}
+            </>
+          ) : (
+            <>
+              {" "}
+              <Route exact path="/" element={<HomePage />} />
+              <Route exact path="/profile" element={<Profile />} />{" "}
+            </>
+          )}
         </Routes>
       </Layout>
     </Box>
