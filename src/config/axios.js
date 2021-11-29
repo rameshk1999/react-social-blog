d93f95bb3722a1cb893448c6cb44bf34edadb44c
rameshk1999 <kasularamesh7337@gmail.com>
2021-11-29 13:53:18 +0530

    checking env

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const LocalURL = "http://localhost:5500/";
 const globalURL = "https://api-social-blog.herokuapp.com/";
+console.log("env", process.env, process.env.NODE_ENV);
 const instance = axios.create({
   baseURL: globalURL,
 });
